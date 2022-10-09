07d7b1289a9b32c971ed4db1aacb9c57051a10f5
emiliangrecu <emiliangrecu25@yahoo.com>
2022-10-09 12:54:32 +0300

    Added class and object.

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace ClassObject
+{
+    class Laptop
+    {
+        string? ssd;
+
+        static void Main(string[] args)
+        {
+
+            // create Laptop object 
+            Laptop dell = new Laptop();
+
+            // set ssd for dell
+            dell.ssd = "Samsung";
+            Console.WriteLine("SSD for Dell: " + dell.ssd);
+
+            // create second object of Laptop
+            Laptop asus = new Laptop();
+
+            // set ssd for Asus
+            asus.ssd = "Seagate";
+            Console.WriteLine("SSD for Asus: " + asus.ssd);
+
+            //Console.ReadLine();
+        }
+    }
+}
