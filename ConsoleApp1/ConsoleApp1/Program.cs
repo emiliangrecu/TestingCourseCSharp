9530c3003bfc736192af92e9264e19ed4c0b6bf4
Emilian Grecu <emilian.grecu@evozon.com>
2022-10-18 10:45:28 +0300

    Class, objects, methods (#4)
    * Working with Methods (Return types)
    
    * small changes
    
    * Multiple Objects of a Class

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -13,7 +13,6 @@ namespace ClassObject
         string type;
         string body;
 
-
         Program(string theBrand, int thePrice, string theColour, string theType, string theBody)
         {
             brand = theBrand;
@@ -23,7 +22,6 @@ namespace ClassObject
             body = theBody;
         }
 
-
         static void Main(string[] args)
         {
             TestClass1 testClass = new TestClass1();
@@ -31,9 +29,20 @@ namespace ClassObject
             testClass.Add();
             testClass.Add(3, 5);
 
+            int sum = testClass.Add(1, 2, 3);
+            Console.WriteLine("Add method with return: " + sum + "\n");
 
             Program car1 = new("BMW", 50000, "black", "diesel", "SUV");
-            Console.WriteLine("Car: " + car1.brand + " " + car1.price + " " + car1.colour + " " + car1.type + " " + car1.body);
+            Console.WriteLine("Car -- Brand: " + car1.brand + "\nPrice: " + car1.price + " EUR \nColour: " + car1.colour + "\nType: " + car1.type + "\nBody: " + car1.body + "\n");
+
+            Employee e1 = new Employee();
+            Employee e2 = new Employee();
+            e1.name = "John";
+            e2.name = "Mike";
+            Console.WriteLine("Name: " + e1.name);
+            e1.work("Coding");
+            Console.WriteLine("Name: " + e2.name);
+            e2.work("Testing");
 
             Console.Read();
         }
--- ConsoleApp1/ConsoleApp1/TestClass1.cs
+++ ConsoleApp1/ConsoleApp1/TestClass1.cs
@@ -11,13 +11,28 @@ namespace ConsoleApp1
     {
         public void Add()
         {
-            Console.WriteLine("Adunare fara parametri");
+            Console.WriteLine("Empty Add method");
         }
 
         public void Add(int num1, int num2)
         {
             int result = num1 + num2;
-            Console.WriteLine("Rezultatul adunarii cu parametri este " + result + "\n");
+            Console.WriteLine("Add method with parameters: " + result + "\n");
+        }
+
+        public int Add(int num1, int num2, int num3)
+        {
+            return num1 + num2 + num3;
+        }
+    }
+
+    class Employee
+    {
+        public string name;
+
+        public void work(string work)
+        {
+            Console.WriteLine("Work: " + work);
         }
     }
 }
