3806972413fa3a077cc0fa70ed72abbab3d60c51
Emilian Grecu <emilian.grecu@evozon.com>
2022-11-15 12:34:54 +0200

    variables and operators (#6)
    * Change price to a double-precision floating point type.
    
    * unary and ternary operators
    
    * Operator Precedence and Associativity
    
    * Another way to read all items of a list
    
    Co-authored-by: emiliangrecu <emiliangrecu25@yahoo.com>

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,7 +10,7 @@ namespace ClassObject
             TestClass1 testClass = new TestClass1();
 
             testClass.Add();
-            testClass.Add(3, 5);
+            testClass.Add(3.25, 5);
 
             int sum = testClass.Add(1, 2, 3);
             Console.WriteLine("Add method with return: " + sum + "\n");
@@ -31,13 +31,26 @@ namespace ClassObject
             Console.WriteLine("Brand: " + carModel.Brand + "\nBody: " + carModel.Body + "\nType: " + carModel.Type + "\nColour: " + carModel.Colour + "\nPrice: " + carModel.Price + " EUR");
             for (int i = 0; i < carModel.extras.Count; i++)
             {
-                Console.WriteLine("Extras: " + carModel.extras[i]);
+                Console.WriteLine("Extras: " + carModel.extras[i] + "\n");
+            }
+
+            //Another way to read all items of a list
+            foreach (var ex in carModel.extras)
+            {
+                Console.WriteLine("Extras: " + ex);
             }
 
             var carDetails = owner.GetCarDetails();
             var det = owner.CarDetails();
-            Console.WriteLine(det[0]);
+            Console.WriteLine("First car extras: " + det[0] + "\n");
 
+            testClass.UnaryOperators(20, true);
+            testClass.TernaryOperator(42);
+            testClass.TernaryOperator(11);
+            testClass.OperatorPrecedence(1, 2, 3);
+
+
+            
             Console.Read();
         }
     }
--- ConsoleApp1/ConsoleApp1/TestClass1.cs
+++ ConsoleApp1/ConsoleApp1/TestClass1.cs
@@ -14,15 +14,46 @@ namespace ConsoleApp1
             Console.WriteLine("Empty Add method");
         }
 
-        public void Add(int num1, int num2)
+        public void Add(double num1, double num2)
         {
-            int result = num1 + num2;
+            double result = num1 + num2;
             Console.WriteLine("Add method with parameters: " + result);
         }
 
         public int Add(int num1, int num2, int num3)
         {
             return num1 + num2 + num3;
+        }
+
+        public  void UnaryOperators(int number, bool flag)
+        {
+            int result;
+
+            result = +number;
+            Console.WriteLine("+number = " + result);
+
+            result = -number;
+            Console.WriteLine("-number = " + result);
+
+            result = ++number;
+            Console.WriteLine("++number = " + result);
+
+            result = --number;
+            Console.WriteLine("--number = " + result);
+
+            Console.WriteLine("!flag = " + (!flag));
+        }
+
+        public void TernaryOperator(int number)
+        {
+            string result = (number % 3 == 0)? "divisible by 3" : "not divisible by 3";
+            Console.WriteLine("{0} is {1}", number, result);
+        }
+
+        public void OperatorPrecedence(int a, int b, int c)
+        {
+            int result = ++a + b / --c;
+            Console.WriteLine("{0} + {1} / {2} = {3}", a, b, c, result);
         }
     }
 }
